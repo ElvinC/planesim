9645d2298b475f8aaa2c6ce0cb3b534bcc29d5a3
ElvinC <ElvinC@users.noreply.github.com>
2019-02-28 17:46:35 +0100

    phaser test

--- static/js/game.js
+++ static/js/game.js
@@ -30,16 +30,50 @@ function create() {
     this.matter.world.setBounds(0, 0, 1000, 500)
 
     // background
+    for (var i = 0; i < 4; i++) {
+        ball = this.matter.add.sprite(Math.random() * 800, Math.random() * 400, "ball")
+        ball.setCircle(100)
 
-    ball = this.matter.add.sprite(300, 200, "ball")
-    ball.setCircle()
+        ball.setFriction(0.005)
+        ball.setBounce(1)
+        ball.setScale(0.5)
+    }
 
-    ball.setFriction(0.005)
-    ball.setBounce(1)
-    ball.setScale(0.5)
-    window.ball = ball;
+    player = this.matter.add.sprite(Math.random() * 800, Math.random() * 400, "ball")
+    player.setCircle(100)
+
+    player.setFixedRotation();
+    player.setAngle(270);
+    player.setFrictionAir(0.05);
+    player.setMass(30);
+    player.setFriction(0.005)
+    player.setBounce(1)
+    player.setScale(0.5)
+
+    window.player = player;
+
+    cursors = this.input.keyboard.createCursorKeys();
+    
 }
 
 function update () {
+    Body = this.matter.Body
 
+    if (cursors.left.isDown)
+    {
+        player.thrustLeft(0.1);
+    }
+    else if (cursors.right.isDown)
+    {
+        player.thrustRight(0.1);
+    }
+
+    if (cursors.up.isDown)
+    {
+        player.thrust(0.1);
+    }
+    else if (cursors.down.isDown)
+    {
+        player.thrustBack(0.1);
+    }
 }
